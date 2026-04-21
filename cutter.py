467de1d5450d05acf8e1f7fd767d68c90eba6f8f
kxstxn <kxstxn@proton.me>
2026-04-21 15:51:07 +0300

    Fixed an issue with non-multiple video lengths

--- cutter.py
+++ cutter.py
@@ -1,32 +1,29 @@
-import platform
-import subprocess
 from moviepy import VideoFileClip
-from pytubefix import YouTube
-from pytubefix.cli import on_progress
 import os
 import subprocess
 import platform
 
-async def cut(url):
+def cut(url):
+    video = VideoFileClip('downloaded/video.mp4')
     print('Start create clips')
     clip_num = 1 #Порядковый номер клипа
     start = 0 #Начальная точка клипа
     lncut = int(input('Enter the clip length in seconds: '))
-    end = lncut #Конечная тока клипа
-    video_len = YouTube(url, on_progress_callback= on_progress).length #Определяем длинну исходного видеоролика
+    end = lncut #Конечная точка клипа
+    video_len = int(video.duration)
     if os.path.isdir('clipped'): #Проверяем существует ли дирректория + создаём если ее нет
         pass
     else:
         os.mkdir('clipped')
-    if end < video_len:
-        while end <= video_len: #Нарезаем оригинальное видео на клипы
-            clip = VideoFileClip('downloaded/video.mp4').subclipped(start, end)
+    if lncut < video_len:
+        while start < video_len:
+            end = min(start + lncut, video_len) #Высчитываем конец клипа для некратных значений
+            clip = video.subclipped(start, end)
             clip.write_videofile(f'clipped/clip{str(clip_num)}.mp4') #Сохраняем каждый клип в заданную дирректорию
             start += lncut
-            end += lncut
             clip_num += 1
         print('The clips have been successfully generated!')
-        path = 'clipped'
+        path = os.path.abspath('clipped')
         if platform.system() == 'Windows':
             subprocess.Popen(['explorer', path])
         elif platform.system() == 'Darwin':
@@ -35,5 +32,7 @@ async def cut(url):
             subprocess.Popen(['xdg-open', path])
     else:
         print('The clip is longer than the video')
+
+
 
 
--- downloader.py
+++ downloader.py
@@ -1,7 +1,7 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-async def download(url): #Загружаем видео с YouTube
+def download(url): #Загружаем видео с YouTube
     yt = YouTube(url, on_progress_callback= on_progress)
     print(f'Downloading "{yt.title}"')
     yd = yt.streams.filter(progressive=True, file_extension='mp4').get_highest_resolution()
