ac6c1c216d27749af02eeb33202c864049956d74
kxstxn <kxstxn@proton.me>
2026-04-21 13:35:13 +0300

    Added the ability to manually set the clip length

--- cutter.py
+++ cutter.py
@@ -1,6 +1,5 @@
 import platform
 import subprocess
-
 from moviepy import VideoFileClip
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
@@ -12,24 +11,29 @@ async def cut(url):
     print('Start create clips')
     clip_num = 1 #Порядковый номер клипа
     start = 0 #Начальная точка клипа
-    end = 60 #Конечная точка клипа
+    lncut = int(input('Enter the clip length in seconds: '))
+    end = lncut #Конечная тока клипа
     video_len = YouTube(url, on_progress_callback= on_progress).length #Определяем длинну исходного видеоролика
     if os.path.isdir('clipped'): #Проверяем существует ли дирректория + создаём если ее нет
         pass
     else:
         os.mkdir('clipped')
-    while end <= video_len: #Нарезаем оришинальное видео на клипы
-        clip = VideoFileClip('downloaded/video.mp4').subclipped(start, end)
-        clip.write_videofile(f'clipped/clip{str(clip_num)}.mp4') #Сохраняем каждый клип в заданную лирректорию
-        start += 60
-        end += 60
-        clip_num += 1
-    print('The clips have been successfully generated!')
-    path = 'clipped'
-    if platform.system() == 'Windows':
-        subprocess.Popen(['explorer', path])
-    elif platform.system() == 'Darwin':
-        subprocess.Popen(['open', path])
+    if end < video_len:
+        while end <= video_len: #Нарезаем оригинальное видео на клипы
+            clip = VideoFileClip('downloaded/video.mp4').subclipped(start, end)
+            clip.write_videofile(f'clipped/clip{str(clip_num)}.mp4') #Сохраняем каждый клип в заданную дирректорию
+            start += lncut
+            end += lncut
+            clip_num += 1
+        print('The clips have been successfully generated!')
+        path = 'clipped'
+        if platform.system() == 'Windows':
+            subprocess.Popen(['explorer', path])
+        elif platform.system() == 'Darwin':
+            subprocess.Popen(['open', path])
+        else:
+            subprocess.Popen(['xdg-open', path])
     else:
-        subprocess.Popen(['xdg-open', path])
+        print('The clip is longer than the video')
+
 
